474b4b554ec69050b5d80ee05b2533abe00a9315
biswash <just11hiro@gmail.com>
2022-12-25 18:44:11 +0530

    added jwt creation and verification

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,6 @@
 const UserRepository =require("../repository/user-repository")
+const {JWT_KEY} = require("../config/serverConfig")
+const jwt = require("jsonwebtoken")
 
 
 
@@ -19,7 +21,28 @@ class UserService{
             throw error;
         }
     }
+   
+      createToken(user){
+        try {
+          const result = jwt.sign(user,JWT_KEY,{expiresIn:'1h'})
 
+           return result  
+        } catch (error) {
+            console.log('error from user-service layer');
+            throw error;
+        }
+      }
+
+      verifyToken(token){
+        try {
+            const verified = jwt.verify(token,JWT_KEY);
+            return verified;
+            
+        } catch (error) {
+            console.log('error from user-service layer :' + error);
+            throw error;
+        }
+      }
     
 }
 
