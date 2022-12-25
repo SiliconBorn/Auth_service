fd8e951b28ad33831a390d92e76f07d6dd481188
biswash <just11hiro@gmail.com>
2022-12-25 19:22:56 +0530

    added api and controller function to serve jwt token for sigin

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -26,9 +26,35 @@ const create = async(req,res)=>{
             success:false
         })
     }
+
+    
 }
 
+const sigIn = async(req,res)=>{
+        try {
+            
+            const response = await userService.signIn(req.body.email,req.body.password)
+        
+
+            return res.status(200).json({
+                data:response,
+                message:"successfully signed in user",
+                err:{},
+                success:true
+            })
+        } catch (error) {
+            console.log('error from user-controller layer');
+            
+            return res.status(500).json({
+                data:{},
+                message:"unable to login user",
+                err:error,
+                success:false
+            })  
+        }
+}
 
 module.exports={
-    create
+    create,
+    sigIn
 }
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -6,7 +6,7 @@ const router = express.Router()
 
 
 router.post('/signup',UserController.create);
-
+router.post('/sigin',UserController.sigIn)
 
 
 module.exports=router
